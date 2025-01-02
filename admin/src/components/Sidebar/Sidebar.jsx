e22a302b52dcdf838c6f84c3f9ad3bf084dfd796
Bogdan <bjankovic16@gmail.com>
2025-01-02 18:04:57 +0100

    6:53

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/admin_assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <NavLink to='/add' className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List items</p>
+        </NavLink>
+        <NavLink to="/orders" className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
